f08a7876be6d5829a18b29692d6d6f39870f7b2e
joshualcaria <alcariajoshua13@gmail.com>
2023-06-08 15:04:53 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primgreen: "#0EB582",
+      },
+    },
+  },
+  plugins: [],
+}
+
